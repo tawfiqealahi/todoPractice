aba925c3ea1ae7f31f6483cef7d4d51ff8e1c079
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-24 08:07:35 -0600

    1

---
+++ main.js
@@ -0,0 +1,9 @@
+// select element
+const form = document.querySelector('#itemForm');
+const inputItem = document.querySelector('#itemInput');
+const itemList = document.querySelector('#itemList');
+const filters = document.querySelectorAll('.nav-item');
+
+// create element and render item
+
+let todoItems = [];
