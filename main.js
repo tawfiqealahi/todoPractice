a6c96b8efc708440a9f599f25cbbb3fa39a859d0
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-30 10:53:09 -0600

    7

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const inputItem = document.querySelector('#itemInput');
 const itemList = document.querySelector('#itemList');
 const filters = document.querySelectorAll('.nav-item');
 
+
+
 // create element and render item
 
 
