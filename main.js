5d9b66348997f9257e94937435a71f3848554754
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-31 10:26:58 -0600

    10

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-var calculateInterest = function (total, years, ratePercent, roundToPlaces) {
-    var interestRate = ((ratePercent/100) + 1);
-    return (total * Math.pow(interestRate, years)).toFixed(roundToPlaces);
-}
 
-var answer = calculateInterest(915, 13, 2, 2);
 
 let calculateInterestRate= function (total, years, ratePercent, roundToPlaces) {
-    let interestRate = ((ratePearcent/100) + 1);
+    let interestRate = ((ratePercent/100) + 1);
     return (total * Math.pow(interestRate, years)).toFixed(roundToPlaces);
 }
+  
+let answer = calculateInterestRate(1000, 5, 2, 20);
+console.log(answer);
 
 
 
@@ -43,15 +41,15 @@ let calculateInterestRate= function (total, years, ratePercent, roundToPlaces) {
 
 
 // select element
-const form = document.querySelector('#itemForm');
+// const form = document.querySelector('#itemForm');
 
-const inputItem = document.querySelector('#itemInput');
-const itemList = document.querySelector('#itemList');
-const filters = document.querySelectorAll('.nav-item');
-
-
-
-// create element and render item
+// const inputItem = document.querySelector('#itemInput');
+// const itemList = document.querySelector('#itemList');
+// const filters = document.querySelectorAll('.nav-item');
 
 
-let todoItems = [];
+
+// // create element and render item
+
+
+// let todoItems = [];
