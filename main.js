0d7c6680f40893b98583304d428f7145c641cd0e
tawfiqealahi <tawfiqealahi99@gmail.com>
2022-01-31 10:22:14 -0600

    9

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+var calculateInterest = function (total, years, ratePercent, roundToPlaces) {
+    var interestRate = ((ratePercent/100) + 1);
+    return (total * Math.pow(interestRate, years)).toFixed(roundToPlaces);
+}
+
+var answer = calculateInterest(915, 13, 2, 2);
+
+let calculateInterestRate= function (total, years, ratePercent, roundToPlaces) {
+    let interestRate = ((ratePearcent/100) + 1);
+    return (total * Math.pow(interestRate, years)).toFixed(roundToPlaces);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // select element
 const form = document.querySelector('#itemForm');
 
